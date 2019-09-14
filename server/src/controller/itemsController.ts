416713687994eaaac3bc2200e119a8176824857b
AVEDIS MAROUKIAN BUDUSSIAN <avedismb@gmail.com>
2019-09-14 17:06:31 -0300

    EP of getItem

--- server/src/controller/itemsController.ts
+++ server/src/controller/itemsController.ts
@@ -1,19 +1,39 @@
 import fetch from 'node-fetch'
-import { ML_SEARC_ITEMS, author } from '../utils/constants'
-import { getCategories, getItem } from '../handlers/itemsHandler'
+import { ML_SEARCH_ITEMS, author, ML_GET_ITEM } from '../utils/constants'
+import { getCategories, getItem as getItemHandler } from '../handlers/itemsHandler'
 import { getItemsResponse } from '../utils/types'
 
 export const getItems = (req: any,res: any) => {
   const search = req.query.search
-  const url = ML_SEARC_ITEMS + search
+  const url = ML_SEARCH_ITEMS + search
   return fetch(url) 
   .then(response => response.json()) 
   .then(result=>{ 
     const { results, filters } = result 
     const categories = getCategories(filters) 
-    let items = results.slice(0,4).map((x:any)=> getItem(x)) 
+    let items = results.slice(0,4).map((x:any)=> getItemHandler(x)) 
     const response: getItemsResponse = { author, categories, items } 
     return res.send(response)
+  })
+  .catch(error => { 
+    return res.send(error) 
+  })
+}
+
+
+export const getItem = (req: any,res: any) => {
+  const id = req.params.id
+  var item: any = ''
+  return fetch('https://api.mercadolibre.com/items/'+id) 
+  .then(response => response.json()) 
+  .then(result=>{ 
+    item = getItemHandler(result) 
+    return fetch('https://api.mercadolibre.com/items/'+id+ '/description')
+  }) 
+  .then(response => response.json()) 
+  .then(result=>{ 
+    item.description = result.plain_text 
+    return res.send(item)
   })
   .catch(error => { 
     return res.send(error) 
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,9 +1,10 @@
 import express = require('express');
-import { getItems } from './controller/itemsController'
+import { getItems, getItem } from './controller/itemsController'
 
 const router = express.Router()
 
 // Items
 router.route('/items').get((req, res) => getItems(req,res))
+router.route('/items/:id').get((req, res) => getItem(req,res))
 
 export default router
